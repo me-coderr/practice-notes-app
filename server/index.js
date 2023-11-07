544f7139011f374fc36668d4cc19f0360c65bada
saeed <me.coderr@gmail.com>
2023-11-07 10:06:32 +0530

    boiler plate code added for client and server side

--- server/index.js
+++ server/index.js
@@ -4,6 +4,21 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", async (req, res) => {
+    res.send("API is running");
+});
+
+// get all notes
+app.get("api/notes", async (req, res) => {
+    res.json(notes);    
+})
+
+// get a particular note based on id
+app.get("api/notes/:id", async (req, res) => {
+    const thisNote = Array.from(notes).find((note) => note._id === req.params.id);
+    res.json(thisNote);
+});
+
 app.listen(PORT, () => {
     console.log(`server listening at ${PORT}`);
-})
+});
